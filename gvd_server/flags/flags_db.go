0df46183f74593b58110f34731c440b7723b6bbe
codingcaius <codingcaius@gmail.com>
2023-07-29 17:42:33 +0800

    命令行参数绑定

---
+++ gvd_server/flags/flags_db.go
@@ -0,0 +1,7 @@
+package flags
+
+import "fmt"
+
+func DB() {
+	fmt.Println("初始化表结构")
+}
--- gvd_server/main.go
+++ gvd_server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"gvd_server/core"
+	"gvd_server/flags"
 	"gvd_server/global"
 	"gvd_server/routers"
 )
@@ -11,6 +12,11 @@ func main() {
 	global.Config = core.InitConfig()
 	global.DB = core.InitMysql()
 	global.Redis = core.InitRedis(0)
+
+	option := flags.Parse()
+	if option.Run() {
+		return
+	}
 
 	router := routers.Routers()
 	addr := global.Config.System.Addr()
