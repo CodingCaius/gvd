a8634270c5207e814947d6a299c14baf47c4f5d2
codingcaius <codingcaius@gmail.com>
2023-08-05 17:48:31 +0800

    用户列表模糊匹配查询

--- gvd_server/api/user_api/user_list.go
+++ gvd_server/api/user_api/user_list.go
@@ -1,6 +1,7 @@
 package user_api
 
 import (
+	"fmt"
 	"gvd_server/global"
 	"gvd_server/models"
 	"gvd_server/service/common/res"
@@ -9,9 +10,9 @@ import (
 )
 
 type UserListRequest struct {
-	Page int `json:"page" form:"page"`
-	Limit int `json:"limit" form:"limit"`
-	Key string `json:"key" form:"key"`
+	Page  int    `json:"page" form:"page"`
+	Limit int    `json:"limit" form:"limit"`
+	Key   string `json:"key" form:"key"`
 }
 
 func (UserApi) UserListView(c *gin.Context) {
@@ -19,11 +20,20 @@ func (UserApi) UserListView(c *gin.Context) {
 	c.ShouldBindQuery(&cr)
 
 	var count int
-	global.DB.Model(models.UserModel{}).Select("count(id)").Scan(&count)
+
+	if cr.Limit < 0 {
+		cr.Limit = 10
+	}
 
 	offset := (cr.Page - 1) * cr.Limit
 
+	//使用参数化查询防止sql注入
+	//避免直接将参数插入sql语句，或者拼接sql语句
+	//前端传过来的值，一定要用 问号
+	query := global.DB.Where("nickName like ?", fmt.Sprintf("%%%s%%", cr.Key))
+
 	var users []models.UserModel
+	global.DB.Model(models.UserModel{}).Where(query).Select("count(id)").Scan(&count)
 	global.DB.Limit(cr.Limit).Offset(offset).Find(&users)
 
 	res.OKWithList(users, count, c)
