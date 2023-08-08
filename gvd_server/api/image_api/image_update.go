4844c98931f9fdac61abee9806da3c80ad099ffc
codingcaius <codingcaius@gmail.com>
2023-08-09 01:37:31 +0800

    图片白名单校验

--- gvd_server/api/image_api/image_update.go
+++ gvd_server/api/image_api/image_update.go
@@ -5,9 +5,19 @@ import (
 	"gvd_server/service/common/res"
 	"gvd_server/utils/jwts"
 	"path"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
+
+var ImageWhiteList = []string{
+	"jpg",
+	"png",
+	"jpeg",
+	"gif",
+	"svg",
+	"webp",
+}
 
 // ImageUploadView 上传图片
 // @Tags 图片管理
@@ -30,6 +40,16 @@ func (ImageApi) ImageUploadView(c *gin.Context) {
 
 	savePath := path.Join("updates", claims.NickName, file.Filename)
 
+	//白名单判断
+	if !InImageWhiteList(file.Filename, ImageWhiteList) {
+		res.FailWithMsg("文件非法", c)
+		return
+	}
+
+	//文件大小判断
+
+	//重复文件判断
+
 	err = c.SaveUploadedFile(file, savePath)
 	if err != nil {
 		global.Log.Errorf("%s 文件保存错误 %s", savePath, err)
@@ -37,5 +57,22 @@ func (ImageApi) ImageUploadView(c *gin.Context) {
 		return
 	}
 
-	res.OK("/" + savePath, "图片上传成功", c)
+	res.OK("/"+savePath, "图片上传成功", c)
+}
+
+//白名单校验
+// InImageWhiteList 判断一个图片是否在白名单中
+func InImageWhiteList(fileName string, whiteList []string) bool {
+	// 截取文件后缀
+	_list := strings.Split(fileName, ".") // xxx  1.2 xxx.png xxx.PNG  xxx.png   xxx.1.2.png  xxxx.png.exe
+	if len(_list) < 2 {
+		return false
+	}
+	suffix := strings.ToLower(_list[len(_list)-1])
+	for _, s := range whiteList {
+		if suffix == s {
+			return true
+		}
+	}
+	return false
 }
