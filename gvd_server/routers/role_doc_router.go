c1bb198892ec7846b3814d0f36d1c56287506aca
codingcaius <codingcaius@gmail.com>
2023-08-28 15:10:59 +0800

    角色文档添加

--- gvd_server/routers/role_doc_router.go
+++ gvd_server/routers/role_doc_router.go
@@ -9,4 +9,7 @@ func (router RouterGroup) RoleDocRouter() {
 	app := api.App.RoleDocApi
 	r := router.Group("role_docs")
 	r.GET(":id", middleware.JwtAdmin(), app.RoleDocListView) // 角色-文档列表
+	r.POST("", middleware.JwtAdmin(), app.RoleDocCreateView)   // 角色-文档 添加
+	//r.DELETE("", middleware.JwtAdmin(), app.RoleDocRemoveView) // 角色-文档 删除
+	//r.GET("info", middleware.JwtAdmin(), app.RoleDocInfoView)  // 角色-文档 信息
 }
