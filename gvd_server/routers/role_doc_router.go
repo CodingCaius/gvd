62cfb28d96f4b141d774c99d71d6daefb9af4de3
codingcaius <codingcaius@gmail.com>
2023-08-28 21:48:24 +0800

    更新单个角色文档

--- gvd_server/routers/role_doc_router.go
+++ gvd_server/routers/role_doc_router.go
@@ -8,8 +8,9 @@ import (
 func (router RouterGroup) RoleDocRouter() {
 	app := api.App.RoleDocApi
 	r := router.Group("role_docs")
-	r.GET(":id", middleware.JwtAdmin(), app.RoleDocListView)   // 角色-文档列表
-	r.POST("", middleware.JwtAdmin(), app.RoleDocCreateView)   // 角色-文档 添加
-	r.DELETE("", middleware.JwtAdmin(), app.RoleDocRemoveView) // 角色-文档 删除
-	r.GET("info", middleware.JwtAdmin(), app.RoleDocInfoView)  // 角色-文档 信息
+	r.GET(":id", middleware.JwtAdmin(), app.RoleDocListView)        // 角色-文档列表
+	r.POST("", middleware.JwtAdmin(), app.RoleDocCreateView)        // 角色-文档 添加
+	r.DELETE("", middleware.JwtAdmin(), app.RoleDocRemoveView)      // 角色-文档 删除
+	r.GET("info", middleware.JwtAdmin(), app.RoleDocInfoView)       // 角色-文档 信息
+	r.PUT("info", middleware.JwtAdmin(), app.RoleDocInfoUpdateView) // 角色-文档 信息更新
 }
